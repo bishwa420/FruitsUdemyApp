3b3f90fc6331244ec367bda600034732f507d595
Bishwajit <bishwa420@gmail.com>
2022-11-12 12:37:22 +0700

    - Onboarding screen added - Fruit view added - Tab view added

---
+++ FruitsUdemyApp/App/FruitsUdemyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsUdemyAppApp.swift
+//  FruitsUdemyApp
+//
+//  Created by Bishwajit Purkaystha on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsUdemyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
---
+++ FruitsUdemyApp/Views/StartButtonView.swift
@@ -0,0 +1,39 @@
+//
+//  StartButtonView.swift
+//  FruitsUdemyApp
+//
+//  Created by Bishwajit Purkaystha on 12/11/22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // PROPERTIES
+    
+    // BODY
+    var body: some View {
+        Button(action: {
+            print("Exit the onboarding")
+        }) {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            } //: HSTACK
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(Capsule().strokeBorder(.white, lineWidth: 1.25))
+        } //: Button
+        .accentColor(.white)
+        
+    }
+}
+
+// PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
