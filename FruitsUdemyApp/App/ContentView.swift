13a45bdb1d86bbcf9fa62cce4f7e040ff5d6d5bf
Bishwajit <bishwa420@gmail.com>
2022-11-25 00:31:51 +0700

    - Navigation UI improved for wide screens (iPads)

--- FruitsUdemyApp/App/ContentView.swift
+++ FruitsUdemyApp/App/ContentView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTIES
+
     @State private var isShowingSettings: Bool = false
-    
+
     var fruits: [Fruit] = fruitsData
-    
+
     // MARK: - BODY
+
     var body: some View {
         NavigationView {
             List {
@@ -27,21 +29,22 @@ struct ContentView: View {
             .navigationTitle("Fruits")
             .navigationBarItems(
                 trailing:
-                    Button(action: {
-                        isShowingSettings = true
-                    }) {
-                        Image(systemName: "slider.horizontal.3")
-                    } //: BUTTON
-                    .sheet(isPresented: $isShowingSettings) {
-                        SettingsView()
-                    }
+                Button(action: {
+                    isShowingSettings = true
+                }) {
+                    Image(systemName: "slider.horizontal.3")
+                } //: BUTTON
+                .sheet(isPresented: $isShowingSettings) {
+                    SettingsView()
+                }
             )
         } //: NAVIGATION
-//        .navigationBarHidden(true)
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 // MARK: - PREVIEW
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(fruits: fruitsData)
